ea63c73f13377eb0a8096df7744a963fb056d18e
SyauqiRevoo1 <148930055+SyauqiRevoo1@users.noreply.github.com>
2023-11-22 11:41:22 +0700

    Add files via upload

---
+++ jobsheet11/Latihan1_Inheritance/src/main/java/com/mycompany/latihan1_inheritance/Kedua.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.latihan1_inheritance;
+
+/**
+ *
+ * Created By_22343075_Syauqi revo mardian
+ */
+public class Kedua extends Pertama {
+    private int b = 8;
+    
+    protected void BacaSuper() {
+        System.out.println("Nilai b = " + b);
+        terprotek();
+        info();
+    }
+}
---
+++ jobsheet11/Latihan1_Inheritance/src/main/java/com/mycompany/latihan1_inheritance/Latihan1_Inheritance.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihan1_inheritance;
+
+/**
+ *
+ * Created By_22343075_Syauqi revo mardian
+ */
+public class Latihan1_Inheritance {
+
+    public static void main(String[] args) {
+        Kedua D2 = new Kedua();
+        D2.BacaSuper();
+        D2.info();
+        
+        Pertama S1 = new Pertama();
+        S1.terprotek();
+        S1.info();
+    }
+}
